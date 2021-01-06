c4c9773748bef699516bf2759a9690ed82c29b52
incesubaris <barisincesu@outlook.com>
2021-01-06 19:38:24 +0300

    syntax fixes

--- Homework3/Solution/main.cpp
+++ Homework3/Solution/main.cpp
@@ -37,23 +37,6 @@ player::player(string newName, int newRebound, int newAssist, int newPoint)
 player::~player()
 {
 }
-/*
-struct Node
-{
-    int data;
-    bool color;
-    Node *left, *right, *parent;
-
-    // Constructor
-    Node(int data)
-    {
-        this->data = data;
-        left = right = parent = NULL;
-        this->color = RED;
-    }
-};
-*/
-
 // Class to represent Red-Black Tree
 class RBTree
 {
@@ -159,24 +142,19 @@ void RBTree::fixViolation(player *&root, player *&pt)
     player *parent_pt = NULL;
     player *grand_parent_pt = NULL;
 
-    while ((pt != root) && (pt->color != BLACK) &&
-           (pt->parent->color == RED))
+    while ((pt != root) && (pt->color != BLACK) && (pt->parent->color == RED))
     {
 
         parent_pt = pt->parent;
         grand_parent_pt = pt->parent->parent;
 
-        /* Case : A 
-			Parent of pt is left child 
-			of Grand-parent of pt */
+        /* Case : A Parent of pt is left child of Grand-parent of pt */
         if (parent_pt == grand_parent_pt->left)
         {
 
             player *uncle_pt = grand_parent_pt->right;
 
-            /* Case : 1 
-			The uncle of pt is also red 
-			Only Recoloring required */
+            /* Case : 1 The uncle of pt is also red Only Recoloring required */
             if (uncle_pt != NULL && uncle_pt->color ==
                                         RED)
             {
@@ -188,9 +166,7 @@ void RBTree::fixViolation(player *&root, player *&pt)
 
             else
             {
-                /* Case : 2 
-				pt is right child of its parent 
-				Left-rotation required */
+                /* Case : 2 pt is right child of its parent Left-rotation required */
                 if (pt == parent_pt->right)
                 {
                     rotateLeft(root, parent_pt);
@@ -198,12 +174,9 @@ void RBTree::fixViolation(player *&root, player *&pt)
                     parent_pt = pt->parent;
                 }
 
-                /* Case : 3 
-				pt is left child of its parent 
-				Right-rotation required */
+                /* Case : 3 pt is left child of its parent Right-rotation required */
                 rotateRight(root, grand_parent_pt);
-                swap(parent_pt->color,
-                     grand_parent_pt->color);
+                swap(parent_pt->color, grand_parent_pt->color);
                 pt = parent_pt;
             }
         }
@@ -215,11 +188,8 @@ void RBTree::fixViolation(player *&root, player *&pt)
         {
             player *uncle_pt = grand_parent_pt->left;
 
-            /* Case : 1 
-				The uncle of pt is also red 
-				Only Recoloring required */
-            if ((uncle_pt != NULL) && (uncle_pt->color ==
-                                       RED))
+            /* Case : 1 The uncle of pt is also red Only Recoloring required */
+            if ((uncle_pt != NULL) && (uncle_pt->color == RED))
             {
                 grand_parent_pt->color = RED;
                 parent_pt->color = BLACK;
@@ -228,9 +198,7 @@ void RBTree::fixViolation(player *&root, player *&pt)
             }
             else
             {
-                /* Case : 2 
-				pt is left child of its parent 
-				Right-rotation required */
+                /* Case : 2 pt is left child of its parent Right-rotation required */
                 if (pt == parent_pt->left)
                 {
                     rotateRight(root, parent_pt);
@@ -239,11 +207,9 @@ void RBTree::fixViolation(player *&root, player *&pt)
                 }
 
                 /* Case : 3 
-				pt is right child of its parent 
-				Left-rotation required */
+				pt is right child of its parent Left-rotation required */
                 rotateLeft(root, grand_parent_pt);
-                swap(parent_pt->color,
-                     grand_parent_pt->color);
+                swap(parent_pt->color, grand_parent_pt->color);
                 pt = parent_pt;
             }
         }
@@ -272,14 +238,37 @@ int main()
 {
     RBTree tree;
 
-    tree.insert("abc", 10, 10, 10);
-    tree.insert("def", 10, 10, 10);
-    tree.insert("ghj", 10, 10, 10);
-    tree.insert("zte", 10, 10, 10);
-    tree.insert("gcc", 10, 10, 10);
-    tree.insert("bac", 10, 10, 10);
-    tree.insert("glk", 10, 10, 10);
-
+    tree.insert("Ali Muhammed", 93, 106, 386);
+    tree.insert("Ekpe Udoh", 241, 68, 376);
+    tree.insert("Jan Vesely", 154, 49, 328);
+    tree.insert("Bogdan Bogdanovic", 84, 80, 321);
+    tree.insert("Gigi Datome", 122, 35, 303);
+    tree.insert("Kostas Sloukas", 62, 130, 268);
+    tree.insert("Nikola Kalinic", 101, 51, 249);
+    tree.insert("James Nunnally", 67, 58, 192);
+    tree.insert("Pero Antic", 75, 19, 130);
+    tree.insert("Melih Mahmutoglu", 10, 11, 79);
+    tree.insert("Ahmet Duverioglu", 12, 1, 30);
+    tree.insert("Anthony Bennett", 9, 2, 12);
+    tree.insert("Baris Hersek", 0, 0, 4);
+    tree.insert("Berk Ugurlu", 1, 2, 2);
+    tree.insert("Egehan Arna", 0, 0, 0);
+    tree.insert("Yordan Minchev", 2, 0, 0);
+    //tree.insert("Jan Vesely", 174, 53, 424);
+    //tree.insert("Brad Wanamaker", 97, 138, 408);
+    //tree.insert("Kostas S l o u k a s", 87, 188, 351);
+    //tree.insert("G i g i Datome", 117, 38, 336);
+    //tree.insert("Nico l o M e l l i", 179, 62, 320);
+    //tree.insert("James Nunnally", 59, 39, 269);
+    //tree.insert("Marko Guduric", 56, 69, 241);
+    //tree.insert("Jason Thompson", 140, 30, 180);
+    //tree.insert("A l i Muhammed", 23, 25, 146);
+    //tree.insert("N i k o l a K a l i n i c", 30, 23, 104);
+    //tree.insert("Ahmet D u v e r i o g l u", 48, 14, 90);
+    //tree.insert("Melih Mahmutoglu", 10, 5, 35);
+    //tree.insert("Sinan Guler", 9, 7, 23);
+    //tree.insert("Egehan Arna", 0, 1, 2);
+    //tree.insert("B a r i s Hersek", 0, 0, 0);
     cout << "Inoder Traversal of Created Tree\n";
     tree.preorder();
 
